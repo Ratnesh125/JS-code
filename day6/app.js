b8887a3a578610c8246dc6824e92313c27791a48
Ratnesh125 <87529730+Ratnesh125@users.noreply.github.com>
2023-06-08 10:45:29 +0530

    Update app.js

--- day6/app.js
+++ day6/app.js
@@ -1,14 +1,13 @@
 const bodyParser=require("body-parser");
-app.use(bodyParser.urlencoded({extended:false}));
-
 const express=require("express");
 const app=express();
+app.use(bodyParser.urlencoded({extended:false}));
 app.listen(3000);
 app.engine("html",require("ejs").renderFile);
 app.set("view engine","html");
 
 const homeRouter=require("./routes/home");
-const homeRouter=require("./routes/login");
+const loginRouter=require("./routes/login");
 
 app.use("/",homeRouter);
-app.use("/",loginRouter);
+app.use("/login",loginRouter);

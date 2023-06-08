2baaf1cd55681c01ff22dc6d765a2da251e26110
Ratnesh125 <87529730+Ratnesh125@users.noreply.github.com>
2023-06-08 10:43:47 +0530

    Update mymodel.js

--- day6/models/mymodel.js
+++ day6/models/mymodel.js
@@ -1,6 +1,6 @@
 const mongoose=require("mongoose");
 mongoose.connect("mongodb://127.0.0.1:27017/mydb");
 var con=mongoose.connection;
-var schema=mongoose.Schema({enroll:String,name:String});
-var MyModel=mongoose.model("mycol",schema)
+var schema=mongoose.Schema({id:String,name:String});
+var MyModel=mongoose.model("users",schema)
 module.exports=MyModel;
